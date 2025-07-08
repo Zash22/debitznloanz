a0c07748bb50249ef9bcdc0eb362f9052d32f348
Nicole Johnson <zash22@gmail.com>
2025-07-08 03:58:01 +0200

    Sanctum for bearer token auth

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Domains\PaymentMethod\DebitCard\Controllers\DebitCardController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/debit-cards', [DebitCardController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('can:viewAny,'.DebitCard::class)->get('/debit-cards', [DebitCardController::class, 'index']);
     Route::middleware('can:create,'.DebitCard::class)->post('/debit-cards', [DebitCardController::class, 'store']);
+});
+
 
