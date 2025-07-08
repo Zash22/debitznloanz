48d582509de9c46a2f87d5efe61d5d6f31294c40
Nicole Johnson <zash22@gmail.com>
2025-07-08 03:44:38 +0200

    renamed test more appropriately

--- tests/Feature/Domains/PaymentMethods/DebitCard/Feature/GetDebitCardsTest.php
+++ tests/Feature/Domains/PaymentMethods/DebitCard/Feature/GetDebitCardsTest.php
@@ -1,33 +1,44 @@
 <?php
 use App\Domains\PaymentMethod\DebitCard\Models\DebitCard;
 use App\Domains\User\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\getJson;
-uses(RefreshDatabase::class);
+//uses(RefreshDatabase::class);
 it('allows a user to list their debit cards', function () {
     /** @var User $user */
     $user = User::factory()->create();
+    $user2 = User::factory()->create();
 
-    // Create debit cards for the authenticated user
-    DebitCard::create([
-        'user_id' => $user->id,
-        'display_name' => 'My First Card',
-        'issuer' => 'Test Bank',
-        'vault_id' => 1
+    Auth::login($user);
+
+    DebitCard::factory()->create([
+        'user_id' => Auth::id(),
+        'display_name' => 'Auth user card 1'
     ]);
-    DebitCard::create([
-        'user_id' => $user->id,
-        'display_name' => 'My Second Card',
-        'issuer' => 'Another Bank',
-        'vault_id' => 2
+
+    DebitCard::factory()->create([
+        'user_id' => Auth::id(),
+        'display_name' => 'Auth user card 2'
     ]);
+
+    DebitCard::factory()->create([
+        'user_id' => $user2->id,
+        'display_name' => 'User 2 card 1'
+    ]);
+
+    DebitCard::factory()->create([
+        'user_id' => $user2->id,
+        'display_name' => 'User 2 card 2'
+    ]);
+
     actingAs($user)
         ->getJson('/api/debit-cards')
         ->assertOk()
         ->assertJsonCount(2, 'data')
-        ->assertJsonPath('data.0.display_name', 'My First Card')
-        ->assertJsonPath('data.1.display_name', 'My Second Card');
+        ->assertJsonPath('data.0.display_name', 'Auth user card 1')
+        ->assertJsonPath('data.1.display_name', 'Auth user card 2');
 });
 it('prevents users from seeing other users debit cards', function () {
     /** @var User $user1 */
