c35ea8a8fee5640525de5222106c14d864e21e0e
Nicole Johnson <zash22@gmail.com>
2025-07-16 22:19:52 +0200

    Fixed construct, add authoriseRequests for policy later on and commented out unusedindex

--- app/Domains/Transaction/Controllers/DebitCardTransactionController.php
+++ app/Domains/Transaction/Controllers/DebitCardTransactionController.php
@@ -2,26 +2,42 @@
 
 namespace App\Domains\Transaction\Controllers;
 
+use App\Domains\PaymentMethod\DebitCard\Resources\DebitCardResource;
+use App\Domains\Transaction\Factories\TransactionTypeFactory;
 use App\Domains\Transaction\Requests\CreateDebitCardTransactionRequest;
 use App\Domains\Transaction\Services\TransactionService;
+use App\Domains\Transaction\TransactionTypes\DebitCardTransactionType;
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class DebitCardTransactionController extends Controller
 {
+    use AuthorizesRequests;
+
     private TransactionService $transactionService;
 
     public function __construct(
-        TransactionService $transactionService
+        TransactionService $transactionService,
+        DebitCardTransactionType $debitCardTransactionType
     ) {
-        $this->transactionService = $transactionService;
-
+        $this->transactionService = new TransactionService(
+            new TransactionTypeFactory(null, null, $debitCardTransactionType)
+        );
     }
+
+//    public function index(Request $request): AnonymousResourceCollection
+//    {
+//        return DebitCardResource::collection(
+//            $this->service->getUserDebitCards($request->user()->id)
+//        );
+//    }
+
 
     public function store(CreateDebitCardTransactionRequest $request): JsonResponse
     {
-
-
         $validated = $request->validated();
 
         $result = $this->transactionService->createOriginatingTransaction(
